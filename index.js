e2166cc1698ca115e3436c6c289a6fd4f9f6a1a2
Bachir <bachirmdicko@gmail.com>
2022-08-13 10:12:21 -0400

    Fixed the typo dot-env to dotenv when requiring

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-require("dot-env").config()
+//Require mpn modules to include into code
+require("dotenv").config()
 const express = require("express")
+
 const app = express()
 
+//Importing router from places.js
+app.use('/places', require('./controllers/places'))
+
+//Creating the Homepage
 app.get("/", (req, res) => {
     res.send("Homepage")
 })
 
+//Creating a catch-all
 app.get('*', (req, res) => {
     res.status(404).send("<h1>404 Page</h1>")
 })
 
+//Keeping the server up listening
 app.listen(process.env.PORT)
