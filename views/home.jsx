f35ec67de5a1ec5f1ac8a2cae2a473aaea29231a
Bachir <bachirmdicko@gmail.com>
2022-08-15 18:22:32 -0400

    Adding Bootstrap grid to index page

--- views/home.jsx
+++ views/home.jsx
@@ -6,7 +6,12 @@ function home () {
         <Def>
             <main>
                 <h1>HOME</h1>
-                <a href="/places"><button className="btn-primary">Places Page</button></a>
+                <div>
+                    <img src='/images/brunch-toast.jpg' alt='Brunch Toast' height= "800px" width="100%"/>
+                    <div>Photo by <a href="https://unsplash.com/@ellaolsson?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Ella Olsson</a> on <a href="https://unsplash.com/s/photos/free-food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                    </div>
+                </div>
+                <a href="/places"><button className="btn btn-primary">Places Page</button></a>
             </main>
         </Def>
     )
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -3,18 +3,27 @@ const Def = require('../default')
 
 function index (data) {
   let placesFormatted = data.places.map((place) => {
-    return (
-      <div>
-        <h2>{place.name}</h2>
-        <img src={place.pic} alt={place.name}/>
-      </div>
-    )
-  })
+  return (
+    <div key = "1" className="col-sm-6">
+      <h2>{place.name}</h2>
+      <p className="text-center">
+        {place.cuisines}
+      </p>
+      <img src={place.pic} alt={place.name} className = "center" width = "100%" />
+      <p className="text-center">
+        Located in {place.city}, {place.state}
+      </p>
+    </div>
+  )
+})
+
   return (
     <Def>
         <main>
-            <h1>PLACES INDEX PAGE</h1>
+            <h1>Places to Rant or Rave About!</h1>
+            <div className="row">
             {placesFormatted}
+            </div>
         </main>
     </Def>
 )
