05f50df76563263b2dcf9cbef9fc06d118cc9d5a
Bachir <bachirmdicko@gmail.com>
2022-08-15 21:20:18 -0400

    New places can be added

---
+++ models/places.js
@@ -0,0 +1,13 @@
+    module.exports = [{
+  name: 'H-Thai-ML',
+  city: 'Seattle',
+  state: 'WA',
+  cuisines: 'Thai, Pan-Asian',
+  pic: '/images/Rest1.jpg'
+}, {
+  name: 'Coding Cat Cafe',
+  city: 'Phoenix',
+  state: 'AZ',
+  cuisines: 'Coffee, Bakery',
+  pic: '/images/Coffee.jpg'
+}]
