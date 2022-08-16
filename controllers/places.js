ff1a4f49c3d020c3de3fe629d75e5c803f5583f1
Bachir <bachirmdicko@gmail.com>
2022-08-15 20:59:09 -0400

    form data is getting through!

--- controllers/places.js
+++ controllers/places.js
@@ -1,5 +1,11 @@
 const router = require('express').Router()
 
+router.post('/', (req, res) => {
+  console.log(req.body)
+  res.send('POST /places')
+})
+
+
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-//Require mpn modules to include into code
-require("dotenv").config()
-const express = require("express")
-
+// Modules and Globals
+require('dotenv').config()
+const express = require('express')
 const app = express()
 
+// Express Settings
+app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true}))
 
-//Importing router from places.js
+// Controllers & Routes
 app.use('/places', require('./controllers/places'))
 
-//Creating the Homepage
-app.get("/", (req, res) => {
-    res.render("home")
+app.get('/', (req, res) => {
+    res.render('home')
 })
 
-//Creating a catch-all
 app.get('*', (req, res) => {
     res.render('error404')
 })
 
-//Keeping the server up listening
+// Listen for Connections
 app.listen(process.env.PORT)
