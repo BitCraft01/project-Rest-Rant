5589b92c16ad577af43ab54f6b5faeab0001204d
Bachir <bachirmdicko@gmail.com>
2022-08-27 13:26:21 -0400

    Added a comments section

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,25 @@ const React = require('react')
 const Def = require('../Default')
 
 function show(data) {
+    let comments = (
+        <h3 className='inactive'>
+            No comments yet!
+        </h3>
+    )
+    if (data.place.comments.length) {
+      comments = data.place.comments.map(c => {
+        return (
+          <div className="border">
+            <h2 className="rant">{c.rant ? 'Rant! ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
+            <h4>{c.content}</h4>
+            <h3>
+              <stong>- {c.author}</stong>
+            </h3>
+            <h4>Rating: {c.stars}</h4>
+          </div>
+        )
+      })
+    }
     return (
         <Def>
             <main>
@@ -32,6 +51,9 @@ function show(data) {
             </form>
                 </div>
                 </div>
+                <hr />
+                <h2>Comments</h2>
+                {comments}
             </main>
             
         </Def>
@@ -39,3 +61,5 @@ function show(data) {
 }
 
 module.exports = show
+
+
