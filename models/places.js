23c106cb0ff0352715e3fe9ccd5435d7c5cf6563
Bachir <bachirmdicko@gmail.com>
2022-08-27 12:27:24 -0400

    showEstablished() completed

--- models/places.js
+++ models/places.js
@@ -2,7 +2,6 @@ const mongoose =require('mongoose')
 
 const placeSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  pic: String,
   pic: { type: String, default: 'http://placekitten.com/350/350'},
   cuisines: { type: String, required: true },
   city: { type: String, default: 'Anytown' },
