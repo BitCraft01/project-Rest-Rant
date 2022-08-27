05a11544a40bceca36d96ddac3b596ca793607b9
Bachir <bachirmdicko@gmail.com>
2022-08-27 12:56:09 -0400

    Added Error message generator when invalid Founded Year is entered

--- controllers/places.js
+++ controllers/places.js
@@ -13,16 +13,27 @@ router.get('/', (req, res) => {
 })
 
 
-router.post('/', (req, res) => {
-  db.Place.create(req.body)
-  .then(() => {
-    res.redirect('/places')
-  })
-  .catch(err=>{
-    console.log('err', err)
+  router.post('/', (req, res) => {
+    db.Place.create(req.body)
+    .then(() => {
+        res.redirect('/places')
+    })
+    .catch(err => {
+        if (err && err.name == 'ValidationError') {
+    let message = 'Validation Error: '
+    for (var field in err.errors) {
+      message += `${field} was ${err.errors[field].value}. `
+      message += `${err.errors[field].message}`
+    }
+    console.log('Validation error message', message)
+    res.render('places/new', { message })
+}
+else {
     res.render('error404')
-  })
+}
+    })
 })
+
 
 router.get('/new', (req, res) => {
   res.render('places/new')
