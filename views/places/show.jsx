17b235935cdff3c5a244d3d33ac68395edbeec44
Bachir <bachirmdicko@gmail.com>
2022-08-29 17:57:19 -0400

    Display of place ratings

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,7 +7,21 @@ function show(data) {
             No comments yet!
         </h3>
     )
+    let rating = (
+        <h3 className="inactive">
+            Not yet rated
+        </h3>
+    )
     if (data.place.comments.length) {
+        let sumRatings = data.place.comments.reduce((tot, c) => {
+            return tot + c.stars
+        }, 0)
+        let averageRating = sumRatings / data.place.comments.length
+        rating = (
+            <h3>
+                {Math.round(averageRating)} stars
+            </h3>
+        )
       comments = data.place.comments.map(c => {
         return (
           <div className="border">
@@ -34,6 +48,11 @@ function show(data) {
                 </div>
                     <div className='col-sm-6'>
                 <h1>{ data.place.name}</h1>
+                <h2>
+                    Rating
+                </h2>
+                {rating}
+                <br />
                 <h2>Description</h2>
                 <h3>
                     {data.place.showEstablished()}
